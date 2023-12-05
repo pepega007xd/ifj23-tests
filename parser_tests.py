27e20286abaf352831fd3a3fb776152169deb2bf
pepega007xd <brablect@gmail.com>
2023-12-05 02:30:28 +0100

    added very basic tests for parser

--- parser_tests.py
+++ parser_tests.py
@@ -1,10 +1,38 @@
 from test_runtime import ErrorType, test, nr_failed
 
+def test_expression_valid(code):
+    code = f"""
+        let result = {code}
+    """
+    test(code, "")
+
+def test_expression_invalid(code):
+    code = f"""
+        let result = {code}
+    """
+    test(code, "", expected_return_code=ErrorType.error_parser)
+
 
 def test_all():
-    test("""
-    1.2.3
-    """, "", expected_return_code=ErrorType.error_lexer)
+    test_expression_valid("1")
+    test_expression_valid("1+1")
+    test_expression_valid("(1+1)")
+    test_expression_valid("2*(2+3)")
+    test_expression_valid("2<3")
+    test_expression_valid("2*((3+4))")
+    test_expression_valid("\"U\"+\"wU\"")
+
+    test_expression_invalid("(2+2(")
+    test_expression_invalid(")2+2)")
+    test_expression_invalid("!2")
+    test_expression_invalid("4!2")
+    test_expression_invalid("1*2*")
+    test_expression_invalid("1++1")
+    test_expression_invalid("+")
+    test_expression_invalid("(1+1")
+    test_expression_invalid("2<3>4")
+    test_expression_invalid("2(3+4)")
+
 
 
 if __name__ == "__main__":
